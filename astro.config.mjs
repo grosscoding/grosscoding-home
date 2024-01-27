d9e297ac0e1a4e49ea49fde434f26bc95cb29fcf
Kevin Gross <kev.gross97@gmail.com>
2024-01-27 12:48:55 +0100

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://grosscoding.github.io',
   base: '/grosscoding-home',
-})
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
